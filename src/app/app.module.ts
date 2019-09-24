ff452a86f8b3c6738882de51e4d3dec2a66200bf
Madrid Recabarren <j.madrid.recabarren@accenture.com>
2019-09-24 12:37:11 -0300

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './Componentes/login-page/login-page.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { RegisterPageComponent } from './Componentes/register-page/register-page.component';
+import { HomePageComponent } from './Componentes/home-page/home-page.component';
+import { NotFoundPageComponent } from './Componentes/not-found-page/not-found-page.component';
+import { ErrorPageComponent } from './Componentes/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    HomePageComponent,
+    NotFoundPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
