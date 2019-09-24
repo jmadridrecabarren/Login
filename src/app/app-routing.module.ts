7dcb7f6fa3f24b1261bd22c0b160b4f49e155666
Madrid Recabarren <j.madrid.recabarren@accenture.com>
2019-09-24 14:19:41 -0300

    Maqueta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './Componentes/login-page/login-page.component';
+import { HomePageComponent } from './Componentes/home-page/home-page.component';
+import { ErrorPageComponent } from './Componentes/error-page/error-page.component';
+import { RegisterPageComponent } from './Componentes/register-page/register-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: HomePageComponent},
+{path: '', component: LoginPageComponent},
+{path: '', component: RegisterPageComponent},
+{path: '**', component: ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
