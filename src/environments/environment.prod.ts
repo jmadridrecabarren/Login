722181b6c837ed6da599a603f60d7878299611ff
Madrid Recabarren <j.madrid.recabarren@accenture.com>
2019-09-24 15:04:19 -0300

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBJPayNvypP3fBSgc1DQVLcRP1vM1t_d4Q",
+    authDomain: "prueba-login-43abf.firebaseapp.com",
+    databaseURL: "https://prueba-login-43abf.firebaseio.com",
+    projectId: "prueba-login-43abf",
+    storageBucket: "",
+    messagingSenderId: "587669336870",
+    appId: "1:587669336870:web:544dd2284ce7aa6a1b47e7",
+    measurementId: "G-L0ZDX9SPXR"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyBJPayNvypP3fBSgc1DQVLcRP1vM1t_d4Q",
+    authDomain: "prueba-login-43abf.firebaseapp.com",
+    databaseURL: "https://prueba-login-43abf.firebaseio.com",
+    projectId: "prueba-login-43abf",
+    storageBucket: "",
+    messagingSenderId: "587669336870",
+    appId: "1:587669336870:web:544dd2284ce7aa6a1b47e7",
+    measurementId: "G-L0ZDX9SPXR"
+  }
 };
 
 /*
